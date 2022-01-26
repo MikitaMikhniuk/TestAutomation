ef79ad752c3807ba2ead11abe46cdbf6b78bdb41
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-01-26 21:05:19 +0300

    testSubject added

---
+++ unitTesting/testSubject.py
@@ -0,0 +1,27 @@
+class Pen:
+    def __init__(self, inkContainerValue = 1000, sizeLetter = 1.0, color = "BLUE"):
+        self.inkContainerValue = inkContainerValue # сколько чернила в ручке
+        self.sizeLetter = sizeLetter # размер букв, которые пишутся ручкой
+        self.color = color # цвет ручки
+
+    def write(self, word):
+        if not self.isWork():
+            return ""
+
+        sizeOfWord = len(word) * self.sizeLetter
+        if sizeOfWord <= self.inkContainerValue:
+            self.inkContainerValue -= sizeOfWord
+            return word
+
+        partOfWord = word[0, self.inkContainerValue]
+        inkContainerValue = 0
+        return partOfWord
+
+    def getColor():
+        return "BLUE"
+
+    def isWork(self):
+        return self.inkContainerValue > 0
+
+    def doSomethingElse(self):
+        print(self.color)
