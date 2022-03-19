428773b2a861a781dac374e7aa664cbebd10efca
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-03-19 20:09:26 +0300

    bdt is working!

--- Selenium/onliner_bdt/onliner/features/catalog_filter.feature
+++ Selenium/onliner_bdt/onliner/features/catalog_filter.feature
@@ -1,24 +1,16 @@
 Feature: Catalog TV Filter
-    In order to filter desired TV's from the Onliner catalog,
     As a regular user
     I want to navigate to TV's catalog page and filter them
     based on some params
 
-    Scenario: Navigate to catalog
+    Scenario: Filter TV's based on params
         Given website "https://www.onliner.by/"
-        When main page is opened
-        Then click top bar "Каталог" button
-        Then verify that main page is "Каталог"
-        
-    Scenario: Navigate to TV's section
         When "Каталог" page is opened
-        Then navigate to "Электроника" -> "Телевидение и видео" -> "Телевизоры"
-        Then verify that page is "Телевизоры"
-        
-    Scenario: Apply filters and verify results
-        When current page is "Телевизоры"
-        Then click on 'Samsung' filter
-        Then set max price "1500"
-        Then set resolution to "1920x1080 (Full HD)"
-        Then set size range between '400' and '500'
-        Then assert filter results
+        And navigate to "Электроника" -> "Телевидение и видео" -> "Телевизоры"
+        Then apply following filters and verify results
+            |   Filter     |   Value               |
+            |   vendor     |   Samsung             |
+            |   max_price  |   2000                |
+            |   resolution |   1920x1080 (Full HD) |
+            |   min_size   |   400                 |
+            |   max_size   |   500                 |
--- Selenium/onliner_bdt/onliner/features/steps/catalog_filter_steps.py
+++ Selenium/onliner_bdt/onliner/features/steps/catalog_filter_steps.py
@@ -1,3 +1,5 @@
+from framework.utils.driver_factory import DriverFactory
+from framework.utils.browser import Browser
 from onliner.page_objects.main_page import MainPage
 from onliner.page_objects.catalog_page import CatalogPage
 from onliner.page_objects.result_page import ResultPage
@@ -6,76 +8,45 @@ from behave import *
 
 @given(u'website "{url}"')
 def step_impl(context, url):
+    context.driver_instance = DriverFactory()
+    context.driver = context.driver_instance.set_up()
+    context.browser = Browser(context.driver)
     context.browser.navigate(url)
-
-
-@when(u'main page is opened')
-def step_impl(context):
-    context.main_page = MainPage(context.driver)
-    context.main_page.verify_main_page()
-
-
-@then(u'click top bar "{section}" button')
-def step_impl(context, section):
-    context.main_page.click_on_catalog_top_bar(section)
-
-
-@then(u'verify that main page is "{header}"')
-def step_imp(context, header):
-    context.catalog_page = CatalogPage(context.driver)
-    context.catalog_page.verify_catalog_page_by_header(header)
 
 
 @when(u'"{header}" page is opened')
 def step_impl(context, header):
+    context.main_page = MainPage(context.driver)
+    context.main_page.verify_main_page()
+    context.main_page.click_on_catalog_top_bar(header)
     context.catalog_page = CatalogPage(context.driver)
     context.catalog_page.verify_catalog_page_by_header(header)
 
 
-@then(u'navigate to "{menu}" -> "{submenu}" -> "{item}"')
+@step(u'navigate to "{menu}" -> "{submenu}" -> "{item}"')
 def step_imp(context, menu, submenu, item):
     context.catalog_page.navigate_to_menu(menu)
     context.catalog_page.navigate_to_submenu(submenu)
     context.catalog_page.click_on_submenu_item(item)
 
 
-@then(u'verify that page is "{section}"')
-def step_impl(context, section):
-    context.result_page = ResultPage(context.driver)
-    context.result_page.verify_result_page_by_header(section)
-
-
-@when(u'current page is "{header}"')
-def step_impl(context, section):
-    context.result_page = ResultPage(context.driver)
-    context.result_page.verify_result_page_by_header(section)
-
-
-@then(u'click on "{filter}" filter')
-def step_impl(context, filter):
-    context.results.click_on_filter_checkbox(filter)
-
-
-@then(u'set max price to "{price}"')
-def step_impl(context, price):
-    context.results.set_max_price(price)
-
-
-@then(u'set size range between "{min_size}" and "{max_size}"')
-def step_impl(context, min_size, max_size):
-    context.results.set_min_size(min_size)
-    context.results.set_max_size(max_size)
-
-
-@then(u'set resolution to "{resolution}"')
-def step_impl(context, resolution):
-    context.results.click_on_filter_checkbox(resolution)
-
-
-@then(u'assert filter results')
+@then(u'apply following filters and verify results')
 def step_impl(context):
-    context.results.wait_for_filter_results()
-    context.results.assert_headers(test_data["VENDOR"])
-    context.results.assert_descriptions(
-        test_data["RESOLUTION"], test_data["MIN_SIZE"], test_data["MAX_SIZE"])
-    context.results.assert_prices(test_data["MAX_PRICE"])
+    context.result_page = ResultPage(context.driver)
+    for row in context.table.rows:
+        header = row[0]
+        value = row[1]
+        if header in ["vendor", "resolution"]:
+            context.result_page.click_on_filter_checkbox(value)
+        if header == "max_price":
+            context.result_page.set_max_price(value)
+        if header == "min_size":
+            context.result_page.set_min_size(value)
+        if header == "max_size":
+            context.result_page.set_max_size(value)
+    context.result_page.wait_for_filter_results()
+    context.res = {row[0]: row[1] for row in context.table.rows}
+    context.result_page.assert_headers(context.res["vendor"])
+    context.result_page.assert_descriptions(
+        context.res["resolution"], context.res["min_size"], context.res["max_size"])
+    context.result_page.assert_prices(context.res["max_price"])
